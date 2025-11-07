6b7202c16ea65df1633a78d004d600763f857d1a
suphakron <suphakroninnorkrew@gmail.com>
2025-11-07 13:24:14 +0700

    fix(log): add logrus in feature product

--- domain/product.go
+++ domain/product.go
@@ -2,21 +2,23 @@ package domain
 
 import (
 	"go-clean-api/entity"
+
+	"github.com/sirupsen/logrus"
 )
 
 type ProductUsecase interface {
-	CreateProduct(product *entity.Product) error
-	EditProduct(product *entity.Product) error
-	GetAllProduct() ([]entity.Product, error)
-	GetShopByUserID(userID uint) (*entity.Shop, error)
-	FindProductByID(id uint) (*entity.Product, error)
-	UpdateProductStock(productID uint, quantity int) error
-	GetProductsByShopID(shopID uint) ([]entity.Product, error)
+	CreateProduct(log *logrus.Entry, product *entity.Product) error
+	EditProduct(log *logrus.Entry, productID uint, shopID int, changes map[string]interface{}) (*entity.Product, error)
+	GetAllProduct(log *logrus.Entry) ([]entity.Product, error)
+	GetShopByUserID(log *logrus.Entry, userID uint) (*entity.Shop, error)
+	FindProductByID(log *logrus.Entry, id uint) (*entity.Product, error)
+	UpdateProductStock(log *logrus.Entry, productID uint, quantity int) error
+	GetProductsByShopID(log *logrus.Entry, shopID uint) ([]entity.Product, error)
 }
 
 type ProductRepository interface {
 	CreateProduct(product *entity.Product) error
-	EditProduct(product *entity.Product) error
+	UpdateProduct(log *logrus.Entry, productID uint, shopID int, changes map[string]interface{}) (*entity.Product, error)
 	GetAllProduct() ([]entity.Product, error)
 	GetShopByUserID(userID uint) (*entity.Shop, error)
 	//GetProductbyID(id uint) (*entity.Product, error)
--- feature/product/usecase/usecase.go
+++ feature/product/usecase/usecase.go
@@ -1,72 +1,106 @@
 package usecase
 
 import (
-	"errors"
+	"go-clean-api/config"
 	"go-clean-api/domain"
 	"go-clean-api/entity"
+
+	"github.com/sirupsen/logrus"
 )
 
 type productUsecase struct {
 	productRepo domain.ProductRepository
-    shopRepo   domain.ShopRepository
+	shopRepo    domain.ShopRepository
+	cfg         config.ToolsConfig
 }
 
 func NewProductUsecase(
-    productRepo domain.ProductRepository,
-    shopRepo domain.ShopRepository,
+	productRepo domain.ProductRepository,
+	shopRepo domain.ShopRepository,
+	cfg config.ToolsConfig,
 ) domain.ProductUsecase {
 	return &productUsecase{
-        productRepo: productRepo,
-        shopRepo:   shopRepo,
+		productRepo: productRepo,
+		shopRepo:    shopRepo,
+		cfg:         cfg,
 	}
 }
 
-func (u *productUsecase) CreateProduct(product *entity.Product) error {
-	return u.productRepo.CreateProduct(product)
-}
-
-func (u *productUsecase) EditProduct(product *entity.Product) error {
-	existing, err := u.productRepo.FindProductByID(uint(product.ID))
-	if err != nil {
-		return err
-	}
-	if existing == nil {
-		return errors.New("product not found")
-	}
-	if existing.ShopID != product.ShopID {
-		return errors.New("not allowed to edit this product")
-	}
-
-	existing.Product_name = product.Product_name
-	existing.Price = product.Price
-	existing.Stock = product.Stock
-
-	if err := u.productRepo.EditProduct(existing); err != nil {
+func (u *productUsecase) CreateProduct(log *logrus.Entry, product *entity.Product) error {
+	if err := u.productRepo.CreateProduct(product); err != nil {
+		log.WithError(err).Error("failed to create product in repo")
 		return err
 	}
 	return nil
 }
 
-func (u *productUsecase) GetAllProduct() ([]entity.Product, error) {
-	return u.productRepo.GetAllProduct()
+func (u *productUsecase) EditProduct(log *logrus.Entry, productID uint, shopID int, changes map[string]interface{}) (*entity.Product, error) {
+
+	log = log.WithFields(logrus.Fields{
+		"product_id": productID,
+		"shop_id":    shopID,
+	})
+
+	updatedProduct, err := u.productRepo.UpdateProduct(log, productID, shopID, changes)
+	if err != nil {
+		log.WithError(err).Error("failed to update product in repo")
+		return nil, err
+	}
+	return updatedProduct, nil
 }
 
-func (u *productUsecase) FindProductByID(id uint) (*entity.Product, error) {
-	return u.productRepo.FindProductByID(id)
+func (u *productUsecase) GetAllProduct(log *logrus.Entry) ([]entity.Product, error) {
+	product, err := u.productRepo.GetAllProduct()
+	if err != nil {
+		log.WithError(err).Error("Failed to get-all product from repo")
+		return nil, err
+	}
+	return product, nil
 }
 
-func (u *productUsecase) UpdateProductStock(productID uint, quantity int) error {
-	return u.productRepo.UpdateProductStock(productID, quantity)
+func (u *productUsecase) FindProductByID(log *logrus.Entry, id uint) (*entity.Product, error) {
+	log = log.WithField("product_id", id)
+	product, err := u.productRepo.FindProductByID(id)
+	if err != nil {
+		log.WithError(err).Error("Failed to get product id from repo")
+		return nil, err
+	}
+	return product, nil
 }
 
-func (u *productUsecase) GetShopByUserID(userID uint) (*entity.Shop, error) {
-    if u.shopRepo != nil {
-        return u.shopRepo.FindByUserID(userID)
-    }
-    // fallback หากยังไม่ได้ inject shopRepo
-    return u.productRepo.GetShopByUserID(userID)
+func (u *productUsecase) UpdateProductStock(log *logrus.Entry, productID uint, quantity int) error {
+	log = log.WithField("product_id", productID)
+
+	if err := u.productRepo.UpdateProductStock(productID, quantity); err != nil {
+		log.WithError(err).Error("Failed to update stock product from repo")
+		return err
+	}
+
+	return nil
 }
 
-func (u *productUsecase) GetProductsByShopID(shopID uint) ([]entity.Product, error) {
-	return u.productRepo.GetProductsByShopID(shopID)
+func (u *productUsecase) GetShopByUserID(log *logrus.Entry, userID uint) (*entity.Shop, error) {
+
+	log = log.WithField("user_id", userID)
+	product, err := u.productRepo.GetShopByUserID(userID)
+
+	if err != nil {
+		log.WithError(err).Error("Failed to get shop by user id from repo")
+		return nil, err
+	}
+
+	return product, nil
+}
+
+func (u *productUsecase) GetProductsByShopID(log *logrus.Entry, shopID uint) ([]entity.Product, error) {
+
+	log = log.WithField("shop_id", shopID)
+	product, err := u.productRepo.GetProductsByShopID(shopID)
+
+	if err != nil {
+		log.WithError(err).Error("failed to get product by shop id from repo")
+		return nil, err
+	}
+
+	return product, nil
 }
